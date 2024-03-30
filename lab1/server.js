802994fb6ecaa556e160789f2a0d2304fbbf4244
omar-a-eid <omerx9988@gmail.com>
2024-03-30 13:59:05 +0200

    Improving the code

--- lab1/server.js
+++ lab1/server.js
@@ -1,49 +1,54 @@
 const http = require("http");
-const fs = require("fs");
+const fs = require("fs").promises;
 
 http
-  .createServer(function (req, res) {
-    let url = req.url;
-    if (url != "/favicon.ico") {
-      let urlParts = url.split("/");
+  .createServer(async function (req, res) {
+    const url = req.url;
 
-      if (urlParts.length < 4) {
-        res.write(
-          "<p>Please Enter an operatons and at least two operands ex. /add/3/4</p>"
-        );
-      } else {
-        let operation = urlParts[1];
-        let result = +urlParts[2];
+    if (url == "/favicon.ico") {
+      res.end();
+      return;
+    }
 
-        for (let i = 3; i < urlParts.length; i++) {
-          if (urlParts[i]) {
-            let operand = +urlParts[i];
-            switch (operation) {
-              case "add":
-                result += operand;
-                break;
-              case "mult":
-                result *= operand;
-                break;
-              case "div":
-                result /= operand;
-                break;
-              case "minus":
-                result -= operand;
-                break;
-              default:
-                break;
-            }
-          }
-        }
-        fs.writeFile("./result.txt", result.toString(), (err) => {
-          if (err) {
-            console.log(err);
-          }
-        });
-        res.write(`Result: ${result}`);
+    const urlParts = url.split("/");
+    if (urlParts.length < 4) {
+      res.write(
+        "<p>Please Enter an operatons and at least two operands ex. /add/3/4</p>"
+      );
+      res.end();
+      return;
+    }
+
+    const operation = urlParts[1];
+    let result = +urlParts[2];
+
+    for (let i = 3; i < urlParts.length; i++) {
+      let operand = +urlParts[i];
+      switch (operation) {
+        case "add":
+          result += operand;
+          break;
+        case "mult":
+          result *= operand;
+          break;
+        case "div":
+          result /= operand;
+          break;
+        case "minus":
+          result -= operand;
+          break;
+        default:
+          break;
       }
     }
+
+    try {
+      await fs.writeFile("./result.txt", result.toString());
+    } catch (err) {
+      console.error("Error", err);
+    }
+
+    res.write(`Result: ${result}`);
     res.end();
   })
   .listen(7000);
